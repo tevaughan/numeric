4fb3660d87bf8c74d540fc1ff65628eb714330fd
Thomas E. Vaughan <tevaugha@ball.com>
2016-08-29 13:54:05 -0600

    Now generate dimensions and units from text file.

--- test/units_test.cpp
+++ test/units_test.cpp
@@ -1,14 +1,14 @@
 
 #include "catch.hpp"
-#include "dimval.hpp"
+#include "units.hpp"
 
 using namespace num;
 using namespace std;
 
-TEST_CASE("Verify basic operation of dimval.", "[dimval]")
+TEST_CASE("Verify basic operation of units.", "[units]")
 {
    length const len1 = meters(3.0);
-   length const len2 = 6.0 * meter;
+   length const len2 = 6.0 * m;
    REQUIRE(len1 / len2 == Approx(0.5));
 }
 
