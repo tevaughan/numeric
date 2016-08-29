57408b433000cd78986259a713933dd1886caf40
Thomas E. Vaughan <tevaugha@ball.com>
2016-08-29 17:17:29 -0600

    Added explicit test involving UTF-8 encoding.

--- test/units_test.cpp
+++ test/units_test.cpp
@@ -25,5 +25,12 @@ TEST_CASE("Verify string conversion.", "[units]")
    oss2 << f2;
    REQUIRE(oss1.str() == "[2 N]");
    REQUIRE(oss2.str() == "[3 kg m s^-2]");
+   microkelvins const t1(2.5);
+   temperature const t2 = 2.5 * μK;
+   ostringstream oss3, oss4;
+   oss3 << t1;
+   oss4 << t2;
+   REQUIRE(oss3.str() == "[2.5 μK]");
+   REQUIRE(oss4.str() == "[2.5e-06 K]");
 }
 
