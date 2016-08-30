5ae270a8e860cc39611d82b98800cd674864f948
Thomas E. Vaughan <tevaugha@ball.com>
2016-08-30 14:24:54 -0600

    Added comments and fleshed out member-func test.

--- test/integral_test.cpp
+++ test/integral_test.cpp
@@ -11,6 +11,7 @@ using namespace std;
 TEST_CASE("Verify integration of lambda.", "[integral]")
 {
    function<double(double)> linear = [](double x) { return x; };
+   // Verify that integral of x of 0 to 1 is 1/2.
    REQUIRE(integral(linear, 0, 1) == Approx(0.5));
 }
 
@@ -18,22 +19,26 @@ area square(length x) { return x * x; }
 
 TEST_CASE("Verify integration of (dimval) global function.", "[integral]")
 {
-   // Verify that integral of x^2 from  0 cm  to  1 cm  is  1/3 cm^3.
    volume const i = integral(square, 0 * cm, 1 * cm);
    ostringstream oss;
    oss << i;
+   // Verify that integral of x^2 from  0 cm  to  1 cm  is  1/3 cm^3.
    REQUIRE(oss.str() == "[3.33333e-07 m^3]");
    REQUIRE(i / cm.pow<3>() == Approx(1.0 / 3.0));
 }
 
-struct foo {
-   double sin(double x) { return std::sin(x); }
+struct my_sin {
+   double freq;
+   double sin(double x) { return std::sin(freq * x); }
+   my_sin(double f = 1.0) : freq(f) {}
 };
 
-TEST_CASE("Verify integration of member function.", "[integral]")
+TEST_CASE("Verify integration of member function (via lambda).", "[integral]")
 {
-   foo f;
+   my_sin f;
    function<double(double)> s = [&f](double x) { return f.sin(x); };
    double const i = integral(s, 0, M_PI);
+   // Verify that integral of sin(x) from 0 to pi is 2.
+   REQUIRE(i == Approx(2.0));
 }
 
