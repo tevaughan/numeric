8cf3202352141b50e78aee527e7ec0427d839d55
Thomas E. Vaughan <tevaugha@ball.com>
2016-09-10 13:21:14 -0600

    Added mainpage docs.

--- src/dimval.hpp
+++ src/dimval.hpp
@@ -235,5 +235,12 @@ namespace num
    };
 }
 
+/// \mainpage
+///
+/// The numeric library provides header-only classes for numerical integration
+/// and for interpolation.  There is also a compiled library for dimensioned
+/// quantities, which can be used with the integrator and with the
+/// interpolator.
+
 #endif // ndef NUMERIC_DIMVAL_HPP
 
